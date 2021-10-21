6a80b964070912a6a64dfe5c5e013199ba837959
victorucan <88557060+victorucan@users.noreply.github.com>
2021-10-20 21:47:56 -0500

    Create instance.tf

---
+++ instance.tf
@@ -0,0 +1,8 @@
+resource "aws_instance" "first-server" {
+    ami = "ami-09e67e426f25ce0d7" 
+    instance_type = "t2.micro"
+    key_name = "vic_Key_New"
+#    count = "5" 
+    tags = {
+        Name = "prueba1"
+    }
