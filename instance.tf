82e0beaac7d18fadd2e93fcbc5f0859d83907777
victorucan <88557060+victorucan@users.noreply.github.com>
2021-10-22 14:39:42 -0500

    Update instance.tf

--- instance.tf
+++ instance.tf
@@ -2,9 +2,9 @@ resource "aws_instance" "first-server" {
     ami = "ami-09e67e426f25ce0d7" 
     instance_type = "t2.micro"
     key_name = "vic_Key_New"
-#    count = "5" 
+    count = "2" 
     tags = {
-        Name = "ansible"
+        Name = "ansible_server"
     }
 }
 resource "aws_instance" "six-server" {
